e1f9b32367065dd35b65a86834cd009d58aeb863
Maximilian Schremser <maxi@schremser.com>
2012-11-08 11:09:26 +0100

    added CONSTANTS for error, ok and wait icons

--- src/com/ibm/swing/HostPinger.java
+++ src/com/ibm/swing/HostPinger.java
@@ -8,8 +8,11 @@ import java.net.Socket;
 import java.util.HashMap;
 
 public class HostPinger extends JFrame implements Runnable {
-    JList list;
-    HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+    private JList list;
+    private HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+    private final static int ERROR_ICON = 0;
+    private final static int OK_ICON = 1;
+    private final static int WAIT_ICON = 2;
 
     public HostPinger(String... hosts) throws HeadlessException {
         super("HostPinger");
@@ -47,7 +50,7 @@ public class HostPinger extends JFrame implements Runnable {
             for (int i = 0; i < list.getModel().getSize(); i++) {
                 String host = list.getModel().getElementAt(i).toString();
                 // put waitIcon for startup
-                hashMap.put(host, 2);
+                hashMap.put(host, WAIT_ICON);
                 list.repaint();
                 Socket socket = null;
                 try {
@@ -62,12 +65,12 @@ public class HostPinger extends JFrame implements Runnable {
                             socket.close();
                         }
                     } catch (IOException ioe) {}
-                    hashMap.put(host, 0);
+                    hashMap.put(host, ERROR_ICON);
                     list.repaint();
                     continue;
                 }
                 // host is reachable, put okIcon and repaint()
-                hashMap.put(host, 1);
+                hashMap.put(host, OK_ICON);
                 list.repaint();
             }
         }
@@ -84,6 +87,10 @@ public class HostPinger extends JFrame implements Runnable {
     }
 
     private class HostCellRenderer extends JLabel implements ListCellRenderer {
+        final ImageIcon errorIcon = createImageIcon("error.png", "Host not reachable");                     // 0
+        final ImageIcon okIcon = createImageIcon("check.png", "Connected to host on port 22");              // 1
+        final ImageIcon waitIcon = createImageIcon("eye.png", "trying to connect to host on port 22");      // 2 || null
+
         protected ImageIcon createImageIcon(String path,
                                             String description) {
             java.net.URL imgURL = getClass().getResource(path);
@@ -95,15 +102,11 @@ public class HostPinger extends JFrame implements Runnable {
             }
         }
 
-        final ImageIcon errorIcon = createImageIcon("error.png", "Host not reachable");                     // 0
-        final ImageIcon okIcon = createImageIcon("check.png", "Connected to host on port 22");              // 1
-        final ImageIcon waitIcon = createImageIcon("eye.png", "trying to connect to host on port 22");      // 2 || null
-
         @Override
         public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             String host = value.toString();
             setText(host);
-            setIcon(hashMap.get(host) == null || hashMap.get(host) == 2 ? waitIcon : hashMap.get(host) == 1 ? okIcon : errorIcon);
+            setIcon(hashMap.get(host) == null || hashMap.get(host) == WAIT_ICON ? waitIcon : hashMap.get(host) == OK_ICON ? okIcon : errorIcon);
             // display Icon on the right side of the label
             setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
             if (isSelected) {
